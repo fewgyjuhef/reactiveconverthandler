f90bf26cd6ac8984ddcf244ba82dd3bf6beeb01f
fewgyjuhef <psjcaytonlan916@hotmail.com>
2024-05-09 10:13:22 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
-function largestNumber(nums) {
-  if (nums.every((num) => num === 0)) return "0";
-  return nums.sort((a, b) => `${b}${a}` - `${a}${b}`).join("");
-}
+const countOccurrences = (arr, value) =>
+  arr.reduce((a, v) => (v === value ? a + 1 : a), 0);
